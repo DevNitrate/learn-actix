bb826a66a520e0f3cdd101bcb053f9df148eff86
DevNitrate <ambroiseinthewild@gmail.com>
2024-08-20 14:58:52 +0200

    working

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
+use actix_files as fs;
+use fs::NamedFile;
 use actix_web::{get, web, App, HttpServer, Responder};
 
-#[get("/{any}")]
-async fn any(any_path: web::Path<String>) -> impl Responder {
-    format!("endpoint: {any_path}")
+#[get("/{file}.html")]
+async fn html(html_file: web::Path<String>) -> actix_web::Result<impl Responder> {
+    let f: std::io::Result<NamedFile> = NamedFile::open(format!("public/{}.html", html_file));
+    if f.is_err() {
+        return Ok(NamedFile::open("public/404.html").unwrap());
+    }
+    Ok(f.unwrap())
+}
+
+#[get("/{file}.css")]
+async fn css(css_file: web::Path<String>) -> actix_web::Result<impl Responder> {
+    let f: std::io::Result<NamedFile> = NamedFile::open(format!("public/{}.css", css_file));
+    if f.is_err() {
+        return Ok(NamedFile::open("public/404.html").unwrap());
+    }
+    Ok(f.unwrap())
 }
 
 #[actix_web::main]
@@ -10,8 +25,8 @@ async fn main() -> std::io::Result<()> {
     let port = 8080;
     println!("Listening on port {port}");
 
-    HttpServer::new(|| App::new().service(any))
-        .bind(("127.0.0.1", port)).unwrap()
+    HttpServer::new(|| App::new().service(html).service(css))
+        .bind(("0.0.0.0", port)).unwrap()
         .run()
         .await
 }
